ee9c521c2896f340ad0c84243bb5000eb7d683b4
Ashlee Ringler <ashleeringler@gmail.com>
2018-12-28 13:55:45 -0700

    Tweaked ExpensesSummary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import getExpensesTotal from '../selectors/expenses-total';
+import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
 
-const ExpensesSummary = (props) => (
-    <div>
-        {
-            props.expenses.length === 1 ? (
-                <p>Viewing 1 expense totalling ${getExpensesTotal(props.expenses)}</p>
-            ) : (
-                <p>Viewing {props.expenses.length} expenses totalling ${getExpensesTotal(props.expenses).toFixed(2)}</p>
-            )
-        }
-    </div>
-);
+
+const ExpensesSummary = ({expenseCount, expensesTotal}) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = expensesTotal.toFixed(2);
+
+    return (
+        <div>
+            <h1>Viewing {expenseCount} {expenseWord} totalling ${formattedExpensesTotal}</h1>
+        </div>
+    )
+}
+
+
+// const ExpensesSummary = (props) => (
+//     <div>
+//         {
+
+//             What I came up with on my own
+//             props.expenses.length === 1 ? (
+//                 <p>Viewing 1 expense totalling ${getExpensesTotal(props.expenses).toFixed(2)}</p>
+//             ) : (
+//                 <p>Viewing {props.expenses.length} expenses totalling ${getExpensesTotal(props.expenses)}</p>
+//             )
+//         }
+//     </div>
+// );
 
 const mapStateToProps = (state) => { //function determines what info from the store we want our component to access
+    const visibleExpenses = selectExpenses(state.expenses, state.filters);
+
     return {
-        expenses: selectExpenses(state.expenses, state.filters)
+        expenseCount: visibleExpenses.length,
+        expensesTotal: selectExpensesTotal(visibleExpenses)
     }
 }
 
